4b4d29d41045c9ba0570e2da3e329a45547d1c59
reyhan muhammad <reyhanmuhammad@reyhans-MacBook-Air.local>
2023-08-24 16:01:53 +0700

    add Test (wip)

---
+++ TestNawaTechUITests/HomePresenterMock.swift
@@ -0,0 +1,8 @@
+//
+//  HomePresenterMock.swift
+//  TestNawaTechUITests
+//
+//  Created by reyhan muhammad on 24/08/23.
+//
+
+import Foundation
---
+++ TestNawaTechUITests/MockDataProvider.swift
@@ -0,0 +1,8 @@
+//
+//  MockDataProvider.swift
+//  TestNawaTechUITests
+//
+//  Created by reyhan muhammad on 24/08/23.
+//
+
+import Foundation
