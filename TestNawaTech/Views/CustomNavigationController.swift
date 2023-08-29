479906643eeb98c213e3b2fda2ea5cd0e1f6d846
reyhan muhammad <reyhanmuhammad@reyhans-MacBook-Air.local>
2023-08-29 09:42:54 +0700

    fix tabBar shows when user cancel swipe back

--- TestNawaTech/Views/CustomNavigationController.swift
+++ TestNawaTech/Views/CustomNavigationController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class CustomNavigationController: UINavigationController{
+class CustomNavigationController: UINavigationController, UINavigationControllerDelegate{
     
     override init(rootViewController: UIViewController) {
         super.init(rootViewController: rootViewController)
@@ -17,13 +17,17 @@ class CustomNavigationController: UINavigationController{
         super.init(coder: aDecoder)
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        delegate = self
+    }
+    
     override func pushViewController(_ viewController: UIViewController, animated: Bool) {
         super.pushViewController(viewController, animated: animated)
         tabBarController?.tabBar.isHidden = true
     }
     
     override func popViewController(animated: Bool) -> UIViewController? {
-        tabBarController?.tabBar.isHidden = viewControllers.count > 2
         return super.popViewController(animated: animated)
     }
     
@@ -32,4 +36,19 @@ class CustomNavigationController: UINavigationController{
         return super.popToRootViewController(animated: animated)
     }
     
+    func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
+    }
+    
+    func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
+        tabBarController?.tabBar.isHidden = viewControllers.count > 1
+    }
+    
+    func navigationController(_ navigationController: UINavigationController, interactionControllerFor animationController: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
+        // Check if the interactive pop gesture was cancelled (user didn't complete the swipe)
+        if interactivePopGestureRecognizer?.state == .cancelled {
+            // Hide the tab bar if needed
+            tabBarController?.tabBar.isHidden = true
+        }
+        return nil
+    }
 }
